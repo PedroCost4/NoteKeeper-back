85bb1b58e1cda5489ae2b192b5e4ed273c86b295
PedroCost4 <pedro.costacdc@gmail.com>
2023-05-02 21:58:29 -0300

    "chore: deleted old js files"

--- src/methods/controller.js
+++
@@ -1,8 +0,0 @@
-import createNote from './create.js';
-import getNotes from './get.js';
-import updateNote from './update.js';
-import deleteNote from './delete.js';
-
-export {createNote, getNotes, updateNote, deleteNote};
-
-
